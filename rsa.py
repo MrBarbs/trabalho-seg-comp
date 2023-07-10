5e080c70081fb2425c764215e58129d22c7d4a02
Ana Braz <98274322+AnaBraz26@users.noreply.github.com>
2023-07-10 08:31:16 -0300

    finalizado

--- rsa.py
+++ rsa.py
@@ -35,17 +35,17 @@ def gerar_primo():
     return next(filter(teste_MillerRabin, iter(maior_impar, 0)))
 
 # https://datatracker.ietf.org/doc/html/rfc8017#appendix-B.2.1
-# def mask(data, seed, mlen):
-#     t = b''
-#     for counter in range(ceil(mlen / 20)):
-#         c = counter.to_bytes(4, "big")
-#         t += sha1(seed + c).digest()
-#     return bytes(map(xor, data, bytes(len(data)) + t[:mlen]))
+def mask(data, seed, mlen):
+    t = b''
+    for counter in range(ceil(mlen / 20)):
+        c = counter.to_bytes(4, "big")
+        t += sha1(seed + c).digest()
+    return bytes(map(xor, data, bytes(len(data)) + t[:mlen]))
 
 #Gerador de chaves
 def gerar_chaves():
     p = gerar_primo()
-    g = gerar_primo()
+    q = gerar_primo()
     n = p * q
     phi_n = (p-1) * (q-1)
     e =  65537
@@ -54,29 +54,29 @@ def gerar_chaves():
     chave_privada = (n,d)
     return (chave_publica, chave_privada)
 
-# def oaep_encode(n, message):
-#     k = (n.bit_length() + 7) // 8
-#     message_len = len(message)
-#     hash_len = 20
-#     lable_hash = b"\xda9\xa3\xee^kK\r2U\xbf\xef\x95`\x18\x90\xaf\xd8\x07\t"
-#     padding_string = b"\x00" * (k - message_len - 2 * hash_len - 2)
-#     data_block = lable_hash + padding_string + b'\x01' + message
-#     seed = urandom(hash_len)
-#     masked_data_block = mask(data_block, seed, k - hash_len - 1)
-#     masked_seed = mask(seed, masked_data_block, hash_len)
-#     return b'\x00' + masked_seed + masked_data_block
+def oaep_encode(n, message):
+    k = (n.bit_length() + 7) // 8
+    message_len = len(message)
+    hash_len = 20
+    lable_hash = b"\xda9\xa3\xee^kK\r2U\xbf\xef\x95`\x18\x90\xaf\xd8\x07\t"
+    padding_string = b"\x00" * (k - message_len - 2 * hash_len - 2)
+    data_block = lable_hash + padding_string + b'\x01' + message
+    seed = urandom(hash_len)
+    masked_data_block = mask(data_block, seed, k - hash_len - 1)
+    masked_seed = mask(seed, masked_data_block, hash_len)
+    return b'\x00' + masked_seed + masked_data_block
 
 
 # # https://datatracker.ietf.org/doc/html/rfc8017#section-7.1.2
-# def oaep_decode(n, em):
-#     k = (n.bit_length() + 7) // 8
-#     hash_len = 20
-#     _, masked_seed, masked_data_block = em[:1], em[1:1 + hash_len], em[1 + hash_len:]
+def oaep_decode(n, em):
+    k = (n.bit_length() + 7) // 8
+    hash_len = 20
+    _, masked_seed, masked_data_block = em[:1], em[1:1 + hash_len], em[1 + hash_len:]
 
-#     seed = mask(masked_seed, masked_data_block, hash_len)
-#     data_block = mask(masked_data_block, seed, k - hash_len - 1)
-#     _, message = data_block.split(b'\x01')
-#     return message
+    seed = mask(masked_seed, masked_data_block, hash_len)
+    data_block = mask(masked_data_block, seed, k - hash_len - 1)
+    _, message = data_block.split(b'\x01')
+    return message
 
 #Realiza a criptografia RSA de uma mensagem usando uma chave fornecida.
 #E retorna a mensagem criptografada como uma sequência de bytes.
@@ -88,12 +88,12 @@ def rsa(chave, mensagem):
     return c.to_bytes(k, "big")
 
 def cifrar(chave, mensagem):
-    crifado = oaep_encode(chave[0], mensagem)
+    cifrado = oaep_encode(chave[0], mensagem)
     return rsa(chave, cifrado)
 
 def decifrar(chave, texto_cifrado):
     cifrado = rsa(chave, texto_cifrado)
-    return oaep_decode(key[0], cifrado)
+    return oaep_decode(chave[0], cifrado)
 
 def assinatura(chave_privada, data):
     hash = sha3_256(data).digest()
@@ -102,4 +102,6 @@ def assinatura(chave_privada, data):
 def verificar(chave_publica, data, assinatura):
     hash = sha3_256(data).digest()
     return rsa(chave_publica, assinatura)[-32:] == hash
+    
+    
     
